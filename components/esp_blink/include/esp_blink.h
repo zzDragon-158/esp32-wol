68e1ebe2b25e7665455cd5fc42c51d63d3ca9404
zzDragon-158 <2211918137@qq.com>
2025-07-17 10:47:44 +0800

    merge blink function

---
+++ components/esp_blink/include/esp_blink.h
@@ -0,0 +1,11 @@
+#ifndef _ESP_BLINK_H_
+#define _ESP_BLINK_H_
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "led_strip.h"
+#include "sdkconfig.h"
+
+void blink_main();
+
+#endif
--- main/app_main.c
+++ main/app_main.c
@@ -2,8 +2,17 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "esp_wol.h"
+#include "esp_blink.h"
 
 void app_main(void)
 {
     esp_wol_init();
+    xTaskCreate(
+        blink_main,
+        "blink",
+        2048,
+        NULL,
+        5,
+        NULL
+    );
 }
